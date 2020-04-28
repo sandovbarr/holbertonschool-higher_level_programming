2a77116c8c1919727fd2354976c8f8fd8369f0a6
Fernando Sandoval <sandovbarr@gmail.com>
2020-04-28 17:29:25 -0500

    [FIX]8task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,9 @@
 def uppercase(str):
     for i in range(len(str)):
         ascichar = ord(str[i])
-        if ((ascichar >= 65 and ascichar <= 90) or
-           (ascichar >= 48 and ascichar <= 57) or ascichar == 32):
-            c = str[i]
+        if ascichar >= 97 and ascichar <= 122:
+            c = chr(ascichar - 32)
         else:
-            c = chr(ord(str[i]) - 32)
+            c = chr(ascichar)
         print("{}".format(c), end="")
     print("")
