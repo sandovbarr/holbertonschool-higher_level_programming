163102c70f324799c722c9ae76e0908b3ab8f7db
Fernando Sandoval <sandovbarr@gmail.com>
2020-04-30 13:55:09 -0500

    [FIX]prints

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,12 @@
 
 import sys
 
-word = "argument"
-word2 = ":"
-
 if len(sys.argv) < 2:
-   print("0 arguments.")
-if len(sys.argv) == 2:
-    print("{:d} {:s}{:s}".format(len(sys.argv) - 1, word, word2))
+    print("{:d} arguments.".format(len(sys.argv) - 1))
+elif len(sys.argv) == 2:
+    print("{:d} argument:".format(len(sys.argv) - 1))
     print("{:d}: {:s}".format(len(sys.argv) - 1, sys.argv[1]))
 elif len(sys.argv) > 2:
-    word = "arguments"
-    word2 = ":"
-    print("{} {}{}".format(len(sys.argv) - 1, word, word2))
+    print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
