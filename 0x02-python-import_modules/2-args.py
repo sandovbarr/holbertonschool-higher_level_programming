6c1d480c36aacfa305f05093425047b54b606631
Fernando Sandoval <sandovbarr@gmail.com>
2020-04-30 13:57:43 -0500

    [FIX]prints

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 
 import sys
 
-if len(sys.argv) < 2:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-elif len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-    print("{:d}: {:s}".format(len(sys.argv) - 1, sys.argv[1]))
-elif len(sys.argv) > 2:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d}: {:s}".format(len(sys.argv) - 1, sys.argv[1]))
+    elif len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
