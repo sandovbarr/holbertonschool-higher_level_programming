7c06521b2aadf8236f3a12e80b42ca45f0f8118a
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-05 15:02:38 -0500

    [FIX]conditionalforimpresions

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for idx in range(len(matrix)):
-        print("{}".format(matrix[idx]), end="\n")
+    for i in matrix:
+        it = 0
+        for j in i:
+            if it == len(i) - 1:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+            it += 1
+        print('')
