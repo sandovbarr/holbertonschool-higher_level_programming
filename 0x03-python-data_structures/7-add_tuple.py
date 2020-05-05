091ae3c24c717b053ee13afd5c3ecc78b2feede5
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-05 18:12:05 -0500

    [ADD]tuplestask

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        tuple_a = (0, 0)
-        tuple_b = (0, 0)
-    elif len(tuple_a) == 1 and len(tuple_b) >= 2:
-        tuple_a = (tuple_a[0], 0)
-    elif len(tuple_b) == 1 and len(tuple_a) >= 2:
-        tuple_b = (tuple_b[0], 0)
-    elif len(tuple_a) == 0 and len(tuple_b) >= 2:
-        tuple_a = (0, 0)
-    elif len(tuple_a) >= 2 and len(tuple_b) == 0:
-        tuple_b = (0, 0)
-    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_c = ((tuple_a[0] if len(tuple_a) >= 1 else 0) +
+               (tuple_b[0] if len(tuple_b) >= 1 else 0),
+               (tuple_a[1] if len(tuple_a) >= 2 else 0) +
+               (tuple_b[1] if len(tuple_b) >= 0 else 0))
     return tuple_c
