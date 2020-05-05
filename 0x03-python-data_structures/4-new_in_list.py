da126e43cb5d5aac1dcdfa77411ff6acdead233e
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-05 14:15:44 -0500

    [FIX]conditional for len

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
 
     new = my_list[:]
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return (new)
     else:
         new[idx] = element
