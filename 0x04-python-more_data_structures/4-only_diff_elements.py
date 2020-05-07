f722172e17b110f27d7ad4900c6373bbfdb869fe
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-07 13:10:21 -0700

    [ADD]4task

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    uniquements = (set_1 | set_2)  - (set_1 & set_2)
+def only_diff_elements(set_1, set_2):
+    uniquements = set_1 ^ set_2
     return uniquements
