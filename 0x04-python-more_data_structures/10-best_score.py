96dcabd9feeeb643e93a502c9f28dfc21e8c75c8
fernando <sandovbarr@gmail.com>
2020-05-07 20:28:07 -0500

    [fix]task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        key = max(a_dictionary) 
-        keyvalue = a_dictionary.get(key)
-        return (key, keyvalue)
-    else:
-        return None
+        return max(a_dictionary, key = a_dictionary.get)
+    return None
