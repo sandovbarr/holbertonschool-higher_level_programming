e433cc3e2e797430b22d134492efab926eda2625
Fernando Sandoval <sandovbarr@gmail.com>
2020-05-07 17:19:28 -0700

    [ADD]best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return (max(a_dictionary))
+    best = max(a_dictionary) if a_dictionary else None
+    return (best)
