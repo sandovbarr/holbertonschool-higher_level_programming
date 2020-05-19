cd7d362232a2b237f9e025fc7675f15398fae731
fernandoSandoval <sandovbarr@gmail.com>
2020-05-19 16:11:13 -0500

    [FIX]identation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """Square class with private attribute
+    Type of size must be an integer
+    otherwise we raise an typeError
+    if size is less than 0
+    we raise a ValueError for size
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
