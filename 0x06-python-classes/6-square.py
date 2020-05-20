0b337450eb09e306b7e4ad58b976b01daefcdbbe
fernandoSandoval <sandovbarr@gmail.com>
2020-05-19 20:02:23 -0500

    [feature]conditionalforprint

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,6 +39,8 @@ class Square:
     @position.setter
     def position(self, value):
         """set the private attribute position"""
+        if type(value) != tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
         if (type(value[0]) != int or type(value[1]) != int):
             raise TypeError('position must be a tuple of 2 positive integers')
         if (value[0] < 0 or value[1] < 0):
