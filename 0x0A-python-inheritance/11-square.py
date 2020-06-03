a4e02cf26ec07c7c00893472210f88edf567cb4c
fernandoSandoval <sandovbarr@gmail.com>
2020-06-02 19:25:28 -0500

    [ADD]feature test1

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -24,6 +24,6 @@ class Square(Rectangle):
     def __init__(self, size):
         super().__init__(size, size)
         self.__size = size
-    
+
     def __str__(self):
         return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
