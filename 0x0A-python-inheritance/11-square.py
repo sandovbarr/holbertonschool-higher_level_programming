5e1a6298dbdb34dcf4903de9a07f82a78234db90
fernandoSandoval <sandovbarr@gmail.com>
2020-06-02 20:29:02 -0500

    [ADD]condition integer

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -22,6 +22,7 @@ class Square(Rectangle):
     the interpretation for square class
     '''
     def __init__(self, size):
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
