741e29da873602cad305469e12a7a08b6ec97116
fernandoSandoval <sandovbarr@gmail.com>
2020-06-03 20:30:59 -0500

    [ADD]tasks

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-'''module with number_of_lines'''
+'''
+    module with number_of_lines
+'''
 
 
 def number_of_lines(filename=""):
-    ''' '''
+    '''
+    Number of lines
+    '''
     cnt_lines = 0
     with open(filename, mode='r', encoding='utf-8') as text_file:
         for lines in text_file:
