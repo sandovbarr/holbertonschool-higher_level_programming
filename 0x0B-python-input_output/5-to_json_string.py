f0dc0ea393c2980f4822d732a4235e41984d0822
fernandoSandoval <sandovbarr@gmail.com>
2020-06-03 20:40:42 -0500

    [FIX]spaces

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 ''' '''
+import json
 
 
 def to_json_string(my_obj):
@@ -7,5 +8,4 @@ def to_json_string(my_obj):
     function that returns the
     JSON representation of an object (string):
     '''
-    import json
     return (json.dumps(my_obj))
