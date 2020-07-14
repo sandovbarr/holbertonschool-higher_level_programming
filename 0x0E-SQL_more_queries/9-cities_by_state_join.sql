48d633f34506bd0278b8c3f34b74bbfdbd85db32
fernandoSandoval <sandovbarr@gmail.com>
2020-07-14 14:59:22 -0500

    [DEL] unneccesary WHERE

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT cities.id, cities.name, states.name FROM cities JOIN states
 ON cities.state_id = states.id
-WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
