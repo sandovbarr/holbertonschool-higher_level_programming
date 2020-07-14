05346ce5085f036c7620f0c7150e2632c0e6de8a
fernandoSandoval <sandovbarr@gmail.com>
2020-07-14 09:47:00 -0500

    [ADD] TEMPERATURES

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+--  script that converts hbtn_0c_0 database to UTF8
+-- (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
