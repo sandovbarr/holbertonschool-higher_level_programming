ea19017c31ecc07b0436e2fad685b87ad43cb34f
fernandoSandoval <sandovbarr@gmail.com>
2020-07-14 13:33:55 -0500

    [ADD] CITIES TABLE

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates the database hbtn_0d_usa and the table states.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE, 
+    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCE hbtn_0d_usa.states(id),
+    name VARCHAR(256) NOT NULL
+    );
