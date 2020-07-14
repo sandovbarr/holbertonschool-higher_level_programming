66e74b31ae1b434e8f6aa5eb412f6d86225c02b6
fernandoSandoval <sandovbarr@gmail.com>
2020-07-13 20:03:53 -0500

    [ADD] Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- script that creates a table second_table in the database
+-- hbtn_0c_0 in your MySQL server and add multiples rows.
+SELECT score,name FROM second_table WHERE score >= 10 ORDER BY score DESC;
