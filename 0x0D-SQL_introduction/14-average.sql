eea3a2e278d1bc8e7c232c215f3525ea9345739f
fernandoSandoval <sandovbarr@gmail.com>
2020-07-13 20:42:01 -0500

    [ADD] AVERAGE

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Write a script that computes the score average
+-- of all records in the table second_tableof the database
+-- hbtn_0c_0 in your MySQL server.
+SELECT AVG(score) AS average FROM second_table;
