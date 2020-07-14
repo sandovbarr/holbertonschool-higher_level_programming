0207428c09a2f9661357ee8e3a0d1a78d61f3ed9
fernandoSandoval <sandovbarr@gmail.com>
2020-07-13 20:22:01 -0500

    [FIX] UPPER

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,3 @@
--- Write a script that updates the score of Bob to 10 in the table second_table.
-UPDATE second_table SET score = 10 where second_table.name = "bob";
+-- Write a script that updates the
+-- score of Bob to 10 in the table second_table.
+UPDATE second_table SET score = 10 WHERE second_table.name = "bob";
