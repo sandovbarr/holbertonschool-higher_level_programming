18fd29983fadb9b08f7f341a27bacfccbacb7942
fernandoSandoval <sandovbarr@gmail.com>
2020-07-14 18:21:51 -0500

    [ADD] List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,7 +5,7 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title as title, tv_genres.name FROM tv_shows
+SELECT tv_shows.title AS title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
