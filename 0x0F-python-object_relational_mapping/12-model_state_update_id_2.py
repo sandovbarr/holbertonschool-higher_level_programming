a2e95988e9e1ab8484a3a2550b421287e4061e0d
Fernando <sandovbarr@gmail.com>
2020-08-09 13:35:54 -0500

    [ADD] update task

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+    script that lists all State objects that contain
+    the letter a from the database hbtn_0e_6_usa
+'''
+from sys import argv
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(user, passwd, db_name))
+    init_session = sessionmaker(bind=engine)
+    conection = init_session()
+    query = (update(State).values(name='New Mexico').where(State.id == 2))
+    conection.execute(query)
+    conection.commit()
+    conection.close()
