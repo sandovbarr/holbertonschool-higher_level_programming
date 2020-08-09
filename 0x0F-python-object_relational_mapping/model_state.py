c66f508581d76d4e49390d69f98fea5b1c0a5d70
fernandoSandoval <sandovbarr@gmail.com>
2020-08-08 19:29:32 -0500

    [ADD] task

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' script that lists all states from the database hbtn_0e_0_usa '''
+
+
+import MySQLdb
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    ''' Empty class that inherits from Base '''
+    tablename = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
