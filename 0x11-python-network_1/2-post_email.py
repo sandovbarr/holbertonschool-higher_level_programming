a554ffb9b43452c77b8e0190f35682b1adad05c9
fernandoSandoval <sandovbarr@gmail.com>
2020-08-24 14:49:00 -0500

    [ADD] new import

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,17 +5,18 @@
     email as a parameter, and displays the body of
     the response (decoded in utf-8)
 '''
-from urllib import request, parse
+from urllib import request
+from urllib import parse
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
-    e_mail = argv[2]
-    values = {'email': e_mail}
+    email = argv[2]
+    values = {'email': email}
     data = parse.urlencode(values)'''needs to be encoded in a standard way'''
-    email = data.encode('ascii') '''data should be bytes'''
-    req = request.Request(url, email) '''make the POST request of data'''
+    data = data.encode('ascii') '''data should be bytes'''
+    req = request.Request(url, data) '''make the POST request of data'''
 
     with request.urlopen(req) as response:
         html = response.read()
