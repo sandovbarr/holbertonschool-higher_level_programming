ea6e271c0d419831476019946ed6803e855a0f35
Fernando <sandovbarr@gmail.com>
2020-09-15 18:59:47 -0500

    [ADD] factorize

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+function factorial (num) {
+  // If the number is less than 0, reject it.
+  if (num < 0) return -1;
+
+  // If the number is 0, its factorial is 1.
+  else if (num === 0) return 1;
+
+  // Otherwise, call the recursive procedure again
+  else return (num * factorial(num - 1));
+}
+
+if (parseInt(process.argv[2])) {
+  console.log(factorial(parseInt(process.argv[2])));
+} else {
+  console.log(NaN);
+}
