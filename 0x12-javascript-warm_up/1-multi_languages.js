3a714eea65d8ad5979f8e8e705edeb128551ebea
Fernando <sandovbarr@gmail.com>
2020-09-15 12:59:09 -0500

    [ADD] multi languages tasks

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const treestrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let iter = 0; iter < treestrings.length; iter++) {
+  console.log(treestrings[iter]);
+}
