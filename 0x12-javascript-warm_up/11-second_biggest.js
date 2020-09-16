9b997ec13b74d37046104559bd5534484ee80603
Fernando <sandovbarr@gmail.com>
2020-09-15 20:51:02 -0500

    [ADD] new feature

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  process.argv = process.argv.slice(2,)
-  process.argv.sort(((a, b) => a - b));
-  console.log(process.argv[2])
+  const arr = process.argv.slice(2);
+  arr.sort(function (a, b) { return a - b; });
+  console.log(arr[arr.length - 2]);
 }
