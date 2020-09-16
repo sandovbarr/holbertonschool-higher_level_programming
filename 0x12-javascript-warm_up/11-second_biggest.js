a69a6e72a8283232137bae665573376981ed5683
Fernando <sandovbarr@gmail.com>
2020-09-15 20:10:49 -0500

    [FIX] sort method with arrow function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const size = process.argv.length;
-  process.argv.sort();
-  console.log(process.argv[size - 2]);
+  process.argv = process.argv.slice(2,)
+  process.argv.sort(((a, b) => a - b));
+  console.log(process.argv[2])
 }
