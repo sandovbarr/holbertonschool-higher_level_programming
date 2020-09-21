fa629d313ed2639957521df8389c8a6a98ca724f
Fernando <sandovbarr@gmail.com>
2020-09-21 12:56:51 -0500

    [ADD] number of ocurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement){
+    let times = 0;
+    for (let iter = 0; iter < list.length; iter++){
+        if (list[iter] === searchElement){
+            times++;
+        }
+    }
+    return times;
+}
