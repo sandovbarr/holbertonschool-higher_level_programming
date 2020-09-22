5f67d54c8777c37943c1ed61dc52c76748e72657
Fernando <sandovbarr@gmail.com>
2020-09-22 16:03:26 -0500

    [ADD] starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const reqUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
 
-request(reqUrl, function (response, error, body) {
-  const info = JSON.parse(body);
-  console.log(info.title);
-});
+request(reqUrl, (response, error, body) => {
+        const info = JSON.parse(body);
+        console.log(info.title);
+    });
