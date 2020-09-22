6039201acdc8c73f3d52d7e680ca507d2a46c6dc
Fernando <sandovbarr@gmail.com>
2020-09-22 14:29:37 -0500

    [ADD] file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
